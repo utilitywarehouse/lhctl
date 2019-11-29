8d6d4c026f75e5f1d427f6038181716aa0ec91b2
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2019-11-29 17:37:29 +0000

    Refactor disable node command and add tests

--- cmd/disable.go
+++ cmd/disable.go
@@ -15,8 +15,9 @@ var disableCmd = &cobra.Command{
 	Use:   "disable",
 	Short: "Disable scheduling on node",
 	Long: `lhctl disable [node-name]
-disables scheduling of replicas on the given node.
-For example:
+disables scheduling of replicas on the given node. If more than one arguments
+are passed the rest will be ugnored.
+example:
 
 # lhctl disable storage-node-0
 `,
@@ -24,9 +25,16 @@ For example:
 		InitManagerClient()
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		validateDisableArgs(cmd, args)
-		disableNode(cmd, args)
-		waitForDisabledNode(cmd, args)
+		node, err := parseDisableArgs(cmd, args)
+		if err != nil {
+			eh.ExitOnError(err)
+		}
+		if err = disableNode(node); err != nil {
+			eh.ExitOnError(err)
+		}
+		if err = waitForDisabledNode(node, 10); err != nil {
+			eh.ExitOnError(err)
+		}
 	},
 }
 
@@ -34,50 +42,45 @@ func init() {
 	rootCmd.AddCommand(disableCmd)
 }
 
-func validateDisableArgs(cmd *cobra.Command, args []string) {
+func parseDisableArgs(cmd *cobra.Command, args []string) (string, error) {
 	if len(args) < 1 {
-		err := errors.New("node name not secified, see lhctl disable -h")
-		eh.ExitOnError(err)
+		return "", errors.New("node name not secified")
 	}
 
-	nodeId := args[0]
-	_, err := mc.GetNode(nodeId)
-	if err != nil {
-		eh.ExitOnError(err, "error getting node info")
-	}
+	return args[0], nil
 }
 
-func disableNode(cmd *cobra.Command, args []string) {
+func disableNode(nodeId string) error {
 
-	nodeId := args[0]
-
-	node, _ := mc.GetNode(nodeId)
+	node, err := mc.GetNode(nodeId)
+	if err != nil {
+		return err
+	}
 
 	update := lh_client.Node{
 		AllowScheduling: false,
 	}
-
-	mc.UpdateNode(node, update)
+	_, err = mc.UpdateNode(node, update)
+	return err
 }
 
-func waitForDisabledNode(cmd *cobra.Command, args []string) {
+func waitForDisabledNode(nodeId string, seconds int) error {
 
-	nodeId := args[0]
-
-	deadline := time.Now().Add(10 * time.Second)
+	deadline := time.Now().Add(time.Duration(seconds) * time.Second)
 	for {
 		node, _ := mc.GetNode(nodeId)
 		if !node.AllowScheduling {
-			fmt.Println("Scheduling disabled for node:", args[0])
-			break
+			fmt.Println("Scheduling disabled for node:", nodeId)
+			return nil
 		}
 		if time.Now().After(deadline) {
-			err := errors.New(fmt.Sprintf(
+			return errors.New(fmt.Sprintf(
 				"could not disable %s",
-				args[0],
+				nodeId,
 			))
-			eh.ExitOnError(err, "timeout")
 		}
 	}
+
+	return nil
 
 }
---
+++ cmd/disable_test.go
@@ -0,0 +1,131 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang/mock/gomock"
+	lh_client "github.com/longhorn/longhorn-manager/client"
+	"github.com/stretchr/testify/assert"
+	"github.com/utilitywarehouse/lhctl/util"
+	"testing"
+)
+
+func TestParseDisableArgs(t *testing.T) {
+
+	// Test: parse with no args returns error
+	expectedErr := errors.New("node name not secified")
+	_, err := parseDisableArgs(disableCmd, []string{})
+
+	assert.Equal(t, expectedErr, err)
+
+	// Test: first argument returned as node id
+	nodeId, err := parseDisableArgs(disableCmd, []string{"node"})
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, nodeId, "node")
+
+	// Test: Rest of arguments ignored
+	nodeId, err = parseDisableArgs(disableCmd, []string{"node", "other"})
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, nodeId, "node")
+}
+
+func TestDisableNode(t *testing.T) {
+	// Mock the manager client
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockClient := util.NewMockManagerClientInterface(mockCtrl)
+	// override the package scoped manager client
+	mc = mockClient
+
+	// Test: Error when get node errors
+	nodeErr := errors.New("node error")
+	mockClient.EXPECT().GetNode("node").Times(1).Return(nil, nodeErr)
+	err := disableNode("node")
+
+	assert.Equal(t, nodeErr, err)
+
+	// Test: Error when update node errors
+	node := &lh_client.Node{}
+	mockClient.EXPECT().GetNode("node").Times(1).Return(node, nil)
+
+	update := lh_client.Node{
+		AllowScheduling: false,
+	}
+	updErr := errors.New("update error")
+	mockClient.EXPECT().UpdateNode(node, update).
+		Times(1).Return(nil, updErr)
+
+	err = disableNode("node")
+	assert.Equal(t, updErr, err)
+
+	// Test: nil when no call errors
+	mockClient.EXPECT().GetNode("node").Times(1).Return(node, nil)
+	mockClient.EXPECT().UpdateNode(node, update).
+		Times(1).Return(node, nil)
+
+	err = disableNode("node")
+	assert.Equal(t, nil, err)
+}
+
+func TestTimeoutWhileWaitingForDisabledNode(t *testing.T) {
+	// Mock the manager client
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockClient := util.NewMockManagerClientInterface(mockCtrl)
+	// override the package scoped manager client
+	mc = mockClient
+
+	// Test: timeout returns error
+	node := &lh_client.Node{}
+	node.AllowScheduling = true
+	mockClient.EXPECT().GetNode("node").AnyTimes().Return(node, nil)
+
+	timeoutErr := errors.New(fmt.Sprintf(
+		"could not disable %s",
+		"node",
+	))
+
+	err := waitForDisabledNode("node", 1)
+	assert.Equal(t, timeoutErr, err)
+
+	// Test: get node error will not exit until timeout
+	node = &lh_client.Node{}
+	nodeErr := errors.New("node error")
+	mockClient.EXPECT().GetNode("node").AnyTimes().Return(node, nodeErr)
+
+	err = waitForDisabledNode("node", 1)
+	assert.Equal(t, timeoutErr, err)
+}
+
+func TestWaitForDisabledNode(t *testing.T) {
+	// Mock the manager client
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockClient := util.NewMockManagerClientInterface(mockCtrl)
+	// override the package scoped manager client
+	mc = mockClient
+
+	// Test: node disabled while trying
+	disabled := &lh_client.Node{}
+	disabled.AllowScheduling = false
+	enabled := &lh_client.Node{}
+	enabled.AllowScheduling = true
+
+	gomock.InOrder(
+		mockClient.EXPECT().GetNode("node").Times(3).
+			Return(enabled, nil),
+		mockClient.EXPECT().GetNode("node").Times(1).
+			Return(disabled, nil),
+	)
+
+	err := waitForDisabledNode("node", 1)
+	assert.Equal(t, nil, err)
+
+	// Test: already disabled
+	mockClient.EXPECT().GetNode("node").Times(1).Return(disabled, nil)
+
+	err = waitForDisabledNode("node", 1)
+	assert.Equal(t, nil, err)
+}
